e77d4f991fb4ebba793c085cec667f58717d69d2
johnkolbert <johnkolbert@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-07-29 19:09:25 +0000

    update to 1.2
    git-svn-id: https://plugins.svn.wordpress.org/absolute-privacy/trunk@140646 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- uninstall.php
+++
@@ -1,9 +0,0 @@
-<?php
-//removes database entries from the plugin on deletion
-
-if( !defined('ABSPATH') && !defined('WP_UNINSTALL_PLUGIN') )
-exit();
-
-delete_option('absolute_privacy');  
-delete_option('absolute_privacy_default');
-?>
