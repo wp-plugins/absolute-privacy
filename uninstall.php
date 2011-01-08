fdc0064775d7ad915b7f36069c867f8a095b559d
johnkolbert <johnkolbert@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-01-08 06:27:23 +0000

    tagging version 2.0
    git-svn-id: https://plugins.svn.wordpress.org/absolute-privacy/tags/2.0@330042 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- uninstall.php
+++ uninstall.php
@@ -1,9 +1,17 @@
 <?php
-//removes database entries from the plugin on deletion
 
-if( !defined('ABSPATH') && !defined('WP_UNINSTALL_PLUGIN') )
-exit();
+/* This file is used when deleting Absolute Privacy from within the plugin page. 
+   The values for delete_option() and remove_role() have been hardcoded here, so
+   they may need some adjusting if you've changed their values in absoluteprivacy.php	
+*/
+   
 
-delete_option('absolute_privacy');  
-delete_option('absolute_privacy_default');
+if ( !defined( 'ABSPATH' ) || !defined( 'WP_UNINSTALL_PLUGIN' ) )
+	exit();	//prevent direct access
+
+
+delete_option( 'absolute-privacy-options' );	//remove the absolute privacy options
+remove_role( 'unapproved' );					//removed the 'unapproved' role
+
+
 ?>
